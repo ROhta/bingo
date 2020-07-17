2901577809d75ffa6088e58cde469c3d1434cb6b
R-O <13445921+ROhta@users.noreply.github.com>
2020-07-17 22:01:15 +0900

    インデント修正 (#16)

--- README.md
+++ README.md
@@ -14,26 +14,26 @@
 ## 機能
 
 - startを押す
-  - ドラムロールが鳴る
-  - 左上部の数字が一定間隔でランダム表示される
-  - start/stopボタンは`stop`となる
-  - ドラムロールが鳴り終わると自動的に数字が選択され、`stop`表示になる
+	- ドラムロールが鳴る
+	- 左上部の数字が一定間隔でランダム表示される
+	- start/stopボタンは`stop`となる
+	- ドラムロールが鳴り終わると自動的に数字が選択され、`stop`表示になる
 - stopを押す
-  - ドラムロールが止まる
-  - シンバルが鳴る
-  - 左上部の数字のランダム表示が止まる
-  - 止まった=選択された数字がHistoriesに加わる
-  - start/stopボタンは`start`となる
+	- ドラムロールを停止する
+	- シンバルが鳴動する
+	- 左上部の数字のランダム表示を停止する
+	- 止まった=選択された数字をHistoriesに追加する
+	- start/stopボタンは`start`となる
 - resetを押す
-  - confirmを出す
-  - Historiesに表示されている数字を全消去する
-  - 左上部の数字は00となる
-  - confirm中、画面の変動は無い
-    - 数字選択中にresetを押下した場合、ドラムロールは鳴り続けるが、数字のランダム表示は停止する
+	- confirmを出す
+	- Historiesに表示されている数字を全消去する
+	- 左上部の数字は00となる
+	- confirm中、画面の変動はない
+		- 数字選択中にresetを押下した場合、ドラムロールは鳴り続けるが、数字のランダム表示は停止する
 - reload時
-  - start/stopボタンは`start`となる
-  - 左上部の数字は00となる
-  - Historiesはresetされず、表示されたままとなる
+	- start/stopボタンは`start`となる
+	- 左上部の数字は00となる
+	- Historiesはresetされず、表示されたままとなる
 
 ## 技術
 
@@ -51,7 +51,7 @@
 
 ### CI/CD
 
-- github actionsで以下を実装
-  - github pagesへのdeploy
-  - deploy可能か、build check
-  - tag付け時に、release noteのdraftを自動生成
+- GitHub actionsで以下を実装
+	- GitHub pagesへのdeploy
+	- deploy可能か、build check
+	- tag付け時に、release noteのdraftを自動生成
--- src/ts/numberList.ts
+++ src/ts/numberList.ts
@@ -1,43 +1,45 @@
 class NumberList {
-  private remainListKey: string
-  private historyListKey: string
-  private allNumberList: number[]
+	private remainListKey: string
+	private historyListKey: string
+	private allNumberList: number[]
 
-  constructor() {
-    this.remainListKey = "remainNumberList"
-    this.historyListKey = "historiesNumberList"
+	constructor() {
+		this.remainListKey = "remainNumberList"
+		this.historyListKey = "historiesNumberList"
 
-    this.allNumberList = []
-    const maxBingoNumber: number = 75
-    for (let i: number = 1; i <= maxBingoNumber; i++) this.allNumberList.push(i)
-  }
+		this.allNumberList = []
+		const maxBingoNumber: number = 75
+		// tslint:disable-next-line: prettier
+		for (let i: number = 1; i <= maxBingoNumber; i++) this.allNumberList.push(i)
+	}
 
-  public getRandomNumber(n: number): number {
-    return Math.floor(Math.random() * n)
-  }
+	public getRandomNumber(n: number): number {
+		return Math.floor(Math.random() * n)
+	}
 
-  public getRemainList(): number[] {
-    const remains: string = localStorage.getItem(this.remainListKey) || ""
-    return remains === "" ? [] : JSON.parse(remains)
-  }
+	public getRemainList(): number[] {
+		const remains: string = localStorage.getItem(this.remainListKey) || ""
+		return remains === "" ? [] : JSON.parse(remains)
+	}
 
-  public getHistoryList(): number[] {
-    const histories: string = localStorage.getItem(this.historyListKey) || ""
-    return histories === "" ? [] : JSON.parse(histories)
-  }
+	public getHistoryList(): number[] {
+		// tslint:disable-next-line: prettier
+		const histories: string = localStorage.getItem(this.historyListKey) || ""
+		return histories === "" ? [] : JSON.parse(histories)
+	}
 
-  public setRemainList(remains: number[]): void {
-    localStorage.setItem(this.remainListKey, JSON.stringify(remains))
-  }
+	public setRemainList(remains: number[]): void {
+		localStorage.setItem(this.remainListKey, JSON.stringify(remains))
+	}
 
-  public setHistoryList(histories: number[]): void {
-    localStorage.setItem(this.historyListKey, JSON.stringify(histories))
-  }
+	public setHistoryList(histories: number[]): void {
+		localStorage.setItem(this.historyListKey, JSON.stringify(histories))
+	}
 
-  public resetLists(): void {
-    localStorage.removeItem(this.historyListKey)
-    localStorage.removeItem(this.remainListKey)
-    this.setRemainList(this.allNumberList)
-    this.setHistoryList([])
-  }
+	public resetLists(): void {
+		localStorage.removeItem(this.historyListKey)
+		localStorage.removeItem(this.remainListKey)
+		this.setRemainList(this.allNumberList)
+		this.setHistoryList([])
+	}
 }
