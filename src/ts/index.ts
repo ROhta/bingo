c94dba9676c3d000c7ed8266c71898d9417b7eb0
RO <13445921+ROhta@users.noreply.github.com>
2022-04-21 12:29:48 +0900

    レスポンシブ対応

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -10,7 +10,7 @@ const doms = new DomManipulation(
     resetButton,
     document.querySelector("#history-title") as HTMLElement,
     document.querySelector("#history-display") as HTMLElement,
-    "col-md-2",
+    "col-2",
     document.querySelector("#drum") as HTMLMediaElement,
     document.querySelector("#cymbals") as HTMLMediaElement,
     150
