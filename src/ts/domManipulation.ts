6239aa78ca871db08557c7a5590b24da0b507562
R-O <13445921+ROhta@users.noreply.github.com>
2022-04-21 14:24:47 +0900

    ライブラリのアップデート (#82)
    * npmモジュールアップデート
    
    * bootstrap update
    
    * 途中経過
    
    * 動くようにした
    
    * class化
    
    * dom要素をclassに持たせる
    
    * 文言はJSで書く
    
    * リファクタリング
    
    * 改行修正
    
    * レスポンシブ対応

---
+++ src/ts/domManipulation.ts
@@ -0,0 +1,110 @@
+// 固定値は予めメンバ変数として保持する
+class DomManipulation {
+	private loadedHistories
+	private isStarted = false
+	private firstDisplayNumber = "00"
+	private startText = "START"
+	private stopText = "STOP"
+	private resetText = "RESET"
+	private historyTitleText = "Hit Numbers"
+
+	constructor(
+		private numbers: NumberList,
+		private bingoNumber: HTMLElement,
+		private startButton: HTMLElement,
+		private resetButton: HTMLElement,
+		private historyTitle: HTMLElement,
+		private historyDisplay: HTMLElement,
+		private historyDisplayClassName: string,
+		private drum: HTMLMediaElement,
+		private cymbals: HTMLMediaElement,
+		private rouletteInterval: number
+	) {
+		if (this.rouletteInterval <= 0) console.error("Interval should be positive number!")
+
+		this.bingoNumber.innerHTML = this.firstDisplayNumber
+		this.startButton.innerHTML = this.startText
+		this.resetButton.innerHTML = this.resetText
+		this.historyTitle.innerHTML = this.historyTitleText
+		this.loadedHistories = this.numbers.getHistoryList()
+
+		if (this.numbers.getRemainList().length === 0 && this.loadedHistories.length === 0) {
+			this.numbers.resetLists()
+		} else {
+			this.loadedHistories.forEach((n: number) => this.addHistory(n))
+		}
+	}
+
+	private makeZeroPaddingNumString = (n: number): string => String(n).padStart(2, "0")
+
+	private addHistory = (n: number): void => {
+		const historyNumber = document.createElement("p")
+		historyNumber.className = this.historyDisplayClassName
+		historyNumber.innerHTML = this.makeZeroPaddingNumString(n)
+		this.historyDisplay.appendChild(historyNumber)
+	}
+
+	private chooseNumber = (): void => {
+		if (!this.isStarted) return
+		this.startButton.innerHTML = this.startText
+
+		const remains = this.numbers.getRemainList()
+		const i = this.numbers.getRandomNumber(remains.length)
+		const randomNum = remains.length === 0 ? -1 : remains[i]
+
+		remains.splice(i, 1)
+		this.numbers.setRemainList(remains)
+
+		const histories = this.numbers.getHistoryList()
+		histories.push(randomNum)
+		this.numbers.setHistoryList(histories)
+
+		this.bingoNumber.innerHTML = this.makeZeroPaddingNumString(randomNum)
+		this.addHistory(randomNum)
+
+		this.drum.pause()
+		this.cymbals.currentTime = 0
+		this.cymbals.play()
+
+		this.isStarted = false
+	}
+
+	private playRoulette = (): void => {
+		if (!this.isStarted) return
+		if (this.drum.currentTime < this.drum.duration) {
+			const rouletteNumbers = this.numbers.getRemainList()
+			this.bingoNumber.innerHTML = this.makeZeroPaddingNumString(rouletteNumbers[this.numbers.getRandomNumber(rouletteNumbers.length)])
+			setTimeout(this.playRoulette, this.rouletteInterval)
+		} else {
+			this.chooseNumber()
+		}
+	}
+
+	public rouletteAction = (): void => {
+		if (this.isStarted) {
+			this.chooseNumber()
+		} else {
+			this.startButton.innerHTML = this.stopText
+
+			this.cymbals.pause()
+			this.drum.currentTime = 0
+			this.drum.play()
+
+			this.isStarted = true
+			this.playRoulette()
+		}
+	}
+
+	public resetAction = (): void => {
+		if (confirm("Do you really want to reset?")) {
+			this.numbers.resetLists()
+			this.isStarted = false
+
+			this.historyDisplay.innerHTML = ""
+			this.drum.pause()
+			this.startButton.innerHTML = this.startText
+			this.bingoNumber.innerHTML = this.firstDisplayNumber
+			this.startButton.focus()
+		}
+	}
+}
---
+++ src/ts/index.ts
@@ -0,0 +1,22 @@
+// DOM
+const startButton = document.querySelector("#start-button") as HTMLElement
+const resetButton = document.querySelector("#reset-button") as HTMLElement
+
+// 可変な設定値を引数として渡す
+const doms = new DomManipulation(
+    new NumberList(),
+    document.querySelector("#bingo-number") as HTMLElement,
+    startButton,
+    resetButton,
+    document.querySelector("#history-title") as HTMLElement,
+    document.querySelector("#history-display") as HTMLElement,
+    "col-2",
+    document.querySelector("#drum") as HTMLMediaElement,
+    document.querySelector("#cymbals") as HTMLMediaElement,
+    150
+)
+
+// main
+startButton.focus()
+startButton.addEventListener("click", (): void => doms.rouletteAction())
+resetButton.addEventListener("click", (): void => doms.resetAction())
